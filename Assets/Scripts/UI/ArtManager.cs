2b58b747986552e5cfdbb91b8c5eb0634f7f2359
Deynuh <60407390+Deynuh@users.noreply.github.com>
2025-11-30 21:48:12 -0800

    put idle sprite art for every call scene

---
+++ Assets/Scripts/UI/ArtManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class ArtManager : MonoBehaviour
+{
+    [SerializeField] private Sprite[] eloSprites;
+    [SerializeField] private Sprite[] umiSprites;
+
+    private VisualElement callScreen;
+    private VisualElement playerCam;
+
+    private int currentSpriteIndex = 0;
+
+    void Start()
+    {
+        callScreen = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("CallScreen");
+        playerCam = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("PlayerCam");
+
+        SetBackgroundSprites(0);
+    }
+
+    public void SetBackgroundSprites(int spriteIndex)
+    {
+        currentSpriteIndex = spriteIndex;
+
+        int eloIndex = Mathf.Clamp(spriteIndex, 0, eloSprites.Length - 1);
+        int umiIndex = Mathf.Clamp(spriteIndex, 0, umiSprites.Length - 1);
+
+        if (callScreen != null && eloIndex < eloSprites.Length && eloSprites[eloIndex] != null)
+        {
+            callScreen.style.backgroundImage = new StyleBackground(eloSprites[eloIndex]);
+        }
+
+        if (playerCam != null && umiIndex < umiSprites.Length && umiSprites[umiIndex] != null)
+        {
+            playerCam.style.backgroundImage = new StyleBackground(umiSprites[umiIndex]);
+        }
+    }
+
+    public void NextSprites()
+    {
+        SetBackgroundSprites(currentSpriteIndex + 1);
+    }
+
+    public void PreviousSprites()
+    {
+        SetBackgroundSprites(currentSpriteIndex - 1);
+    }
+
+    public void ResetToFirst()
+    {
+        SetBackgroundSprites(0);
+    }
+}
